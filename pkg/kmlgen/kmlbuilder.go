dabbafd1b7b5f38b6a48b27bdc9912ca3c38897b
Jonathan Hudson <jh+github@daria.co.uk>
2024-06-12 11:16:46 +0100

    use cli settings in (all) cases

--- pkg/kmlgen/kmlbuilder.go
+++ pkg/kmlgen/kmlbuilder.go
@@ -512,6 +512,7 @@ func GenerateMissionOnly(outfn string, gv func() string) {
 	if len(options.Config.Cli) > 0 {
 		sfx = Generate_cli_kml(options.Config.Cli, fb)
 	}
+
 	kname := filepath.Base(options.Config.Mission)
 	desc := fmt.Sprintf("Generator: %s", gv())
 	d := kml.Folder(kml.Name(kname)).Add(kml.Description(desc)).Add(kml.Open(true))
@@ -569,7 +570,13 @@ func GenerateKML(hpos types.HomeRec, rec types.LogRec, outfn string,
 	d := kml.Folder(kml.Name(meta.LogName())).Add(kml.Description(desc)).Add(kml.Open(true))
 	d.Add(add_ground_track(rec))
 
+	var sfx []kml.Element
+
 	fb := geo.Getfrobnication()
+	if len(options.Config.Cli) > 0 {
+		sfx = Generate_cli_kml(options.Config.Cli, fb)
+	}
+
 	if len(options.Config.Mission) > 0 {
 		_, mm, err := mission.Read_Mission_File(options.Config.Mission)
 		if err == nil {
@@ -599,7 +606,6 @@ func GenerateKML(hpos types.HomeRec, rec types.LogRec, outfn string,
 	}
 
 	if len(options.Config.Cli) > 0 {
-		sfx := Generate_cli_kml(options.Config.Cli, fb)
 		for _, s := range sfx {
 			d.Add(s)
 		}
