3fd3134d0dcb9b0327fe79d79ee9b05bb6a8f1f0
Jonathan Hudson <jh+github@daria.co.uk>
2025-01-07 21:00:58 +0000

    fix meson ".exe" test (Windows, again)

--- meson/post_install.py
+++ meson/post_install.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 ## python, so it (maybe) works on Windows
+import platform
 import os
+import sys
+
 exe=''
+if platform.system() == "Windows":
+    exe = ".exe"
+    print("EXE from OS", file=sys.stderr)
+
 if "EXE" in os.environ:
     exe = os.environ.get("EXE")
-    
+    print("EXE from ENV", file=sys.stderr)
+
 inspath = os.path.join(os.environ['MESON_INSTALL_PREFIX'],'bin')
 dst = os.path.join(inspath, 'fl2ltm'+exe)
 src = os.path.join(inspath, 'fl2mqtt'+exe)
